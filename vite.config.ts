2a37715e30551e195a2fd843cde4afbc576cacd5
Levi Schouten <levi.schouten.werk@gmail.com>
2023-10-29 13:39:24 +0100

    updated vite config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: "0.0.0.0",
-  },
+  base: "/free-resume-builder/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
